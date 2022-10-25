e92253b3291c3c9bc3f6da71ddd768d6eac4c6d2
mitulchy07 <mitul9616@gmail.com>
2022-10-25 12:52:41 +0200

    Update of router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  return (
+    <div className='App'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <button className='btn btn-accent'>Button</button>
+    </div>
+  );
 }
 
 export default App;
